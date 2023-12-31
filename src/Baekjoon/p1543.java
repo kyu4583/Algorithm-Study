1dd9d76edbb51258bfc11df64cc58ed32ed9e8ca
Sungkyu <kxu4583@naver.com>
2023-12-31 23:29:16 +0900

    백준: p1543 <문서 검색> 정답

--- src/Baekjoon/p1543.java
+++ src/Baekjoon/p1543.java
@@ -1,5 +1,32 @@
 package Baekjoon;
 
-public class p1543 {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
+public class p1543 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String dividend = br.readLine();
+        String divisor = br.readLine();
+        String target = "";
+        int answer = 0;
+
+        for (int i = 0; i < dividend.length(); i++) {
+            target += dividend.charAt(i);
+            if (target.equals(divisor)) {
+                answer += 1;
+                target = "";
+            }
+            if (target.length() >= divisor.length()) {
+                target = target.substring(1);
+            }
+        }
+
+        bw.write(Integer.toString(answer));
+        bw.flush();
+
+    }
 }
