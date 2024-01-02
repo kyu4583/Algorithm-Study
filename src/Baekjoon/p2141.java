1405835ee3ea38e4f5cc0f96e6c5412dda439d9c
Sungkyu <kxu4583@naver.com>
2024-01-02 22:25:18 +0900

    백준: p2141 우체국 정답

--- src/Baekjoon/p2141.java
+++ src/Baekjoon/p2141.java
@@ -4,26 +4,42 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
-import static java.lang.Math.round;
 
 public class p2141 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int m = 0;
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
+        int N = Integer.parseInt(br.readLine());
+        int M = 0;
+        long rightA = 0;    //오버플로 ㅋㅋㅋㅋㅋㅋ
+        List<List<Integer>> map = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
             String[] chars = br.readLine().split(" ");
-            int x = Integer.parseInt(chars[0]);
-            int a = Integer.parseInt(chars[1]);
-            sum += x * a;
-            m += a;
+            int X = Integer.parseInt(chars[0]);
+            int A = Integer.parseInt(chars[1]);
+            rightA += A;
+            map.add(List.of(X, A));
         }
 
-        bw.write(Integer.toString((int) round((double) sum / m)));
+        map.sort((o1, o2) -> o1.get(0).compareTo(o2.get(0)));
+
+        long leftA = 0;
+        for (int i = 0; ; i++) {
+            int nowA = map.get(i).get(1);
+            leftA += nowA;
+            rightA -= nowA;
+            if (leftA >= rightA) {
+                bw.write(Integer.toString(map.get(i).get(0)));
+                break;
+            }
+        }
+
         bw.flush();
     }
 }
