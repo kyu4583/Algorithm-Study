e982df6fcd62890be0e5c1e1929e74c027b42177
신성규 <kxu4583@naver.com>
2024-01-06 23:24:50 +0900

    백준: p11501 <주식> 시간초과1

---
+++ src/Baekjoon/p11501.java
@@ -0,0 +1,68 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class p11501 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            List<Integer> lst = new ArrayList<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                lst.add(Integer.parseInt(st.nextToken()));
+            }
+
+            List<Integer> action = new ArrayList<>(Collections.nCopies(N, 0));
+            int start = 0;
+            for (int max = maxIndex(lst, 0); max < lst.size(); max = maxIndex(lst, max + 1)) {
+                for (int j = start; j < max; j++) {
+                    action.remove(j);
+                    action.add(j, 1);
+                }
+                action.remove(max);
+                action.add(max, 2);
+                start = max + 1;
+            }
+
+            int answer = 0;
+            int purchase = 0;
+            for (int j = 0; j < N; j++) {
+                if (action.get(j) == 1) {
+                    answer -= lst.get(j);
+                    purchase += 1;
+                }
+                if (action.get(j) == 2) {
+                    answer += lst.get(j) * purchase;
+                    purchase = 0;
+                }
+            }
+
+            bw.write(Integer.toString(answer) + "\n");
+        }
+
+        bw.flush();
+
+    }
+
+    public static int maxIndex(List<Integer> lst, int start) {
+        if (start > lst.size() - 1) {
+            return start;
+        }
+        int max = start;
+        for (int i = start; i < lst.size(); i++) {
+            if (lst.get(i) > lst.get(max)) {
+                max = i;
+            }
+        }
+        return max;
+    }
+}
