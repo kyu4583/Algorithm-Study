9ea559d6c7b719b3e9c86f74d9ad5019079c90e6
신성규 <kxu4583@naver.com>
2024-01-10 14:06:21 +0900

    백준: p12865 <평범한 배낭> 시간초과,오답1
    시간초과, 물건을 중복으로 집어넣는 문제도 있음.

--- src/Baekjoon/p12865.java
+++ src/Baekjoon/p12865.java
@@ -4,22 +4,28 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class p12865 {
+    static List<Component> components = new ArrayList<>();
+    static int[] maxValueTable;
+    static int N;
+    static int K;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        List<Component> components = new ArrayList<>();
+        maxValueTable = new int[K];
+        maxValueTable[0] = 0;
+        for (int i=1; i<K; i++) {
+            maxValueTable[i] = -1;
+        }
 
         for (int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -27,14 +33,31 @@ public class p12865 {
             components.add(component);
         }
 
-
-
-
-        bw.write(components.toString());
+        bw.write(Integer.toString(maxValue(K)));
         bw.flush();
     }
 
-    public static class Component{
+    private static int maxValue(int weight) {
+        if (weight == 0) {
+            return 0;
+        }
+        int max = maxValue(weight - 1);
+        for (int i=0; i<N; i++) {
+            Component nowComponent = components.get(i);
+            int targetWeight = weight - nowComponent.getWeight();
+            if(targetWeight >= 0) {
+                if(maxValueTable[targetWeight] == -1) {
+                    maxValueTable[targetWeight] = maxValue(targetWeight);
+                }
+                if(maxValueTable[targetWeight] + nowComponent.getValue() > max) {
+                    max = maxValueTable[targetWeight] + nowComponent.getValue();
+                }
+            }
+        }
+        return max;
+    }
+
+    private static class Component{
         int weight;
         int value;
 
