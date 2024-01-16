d00484d844761214ce673500e4ef792f542f912d
신성규 <kxu4583@naver.com>
2024-01-17 00:26:56 +0900

    백준: p2448 <별찍기 - 11> 정답

--- src/Baekjoon/p2448.java
+++ src/Baekjoon/p2448.java
@@ -14,19 +14,51 @@ public class p2448 {
 
         int N = Integer.parseInt(br.readLine());
 
-        table = new boolean[N][(N * 2) - 1];
+        table = new boolean[N][N * 2];
         int degree = (int) baseLog(N / 3, 2);
 
         scatch(0, N - 1, degree);
 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < (N * 2) - 1; j++) {
+                if (table[i][j]) {
+                    bw.write('*');
+                } else {
+                    bw.write(' ');
+                }
+            }
+            bw.write('\n');
+        }
 
+        bw.flush();
     }
 
     static double baseLog(double x, double base) {
         return Math.log10(x) / Math.log10(base);
     }
 
+    static void scatch(int startI, int startJ, int degree) {
+        if (degree == 0) {
+            table[startI][startJ] = true;
+            table[startI + 1][startJ - 1] = true;
+            table[startI + 1][startJ + 1] = true;
+            for (int j = startJ - 2; j <= startJ + 2; j++) {
+                table[startI + 2][j] = true;
+            }
+        } else {
+            scatch(startI, startJ, degree - 1);
+            scatch(startI + getHeightFromDegree(degree - 1), startJ - getWidthFromDegree(degree - 1) / 2, degree - 1);
+            scatch(startI + getHeightFromDegree(degree - 1), startJ + getWidthFromDegree(degree - 1) / 2, degree - 1);
+        }
 
+    }
 
+    static int getHeightFromDegree(int degree) {
+        return 3 * (int) Math.pow(2, degree);
+    }
+
+    static int getWidthFromDegree(int degree) {
+        return 3 * (int) Math.pow(2, degree + 1);
+    }
 
 }
